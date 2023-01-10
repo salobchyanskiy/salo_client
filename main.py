78afa10d0d53c685bea14b500fe0b7888dfda588
salobchyanskiy <salobchyanskiy@Gmail.com>
2023-01-10 16:12:04 +0300

    Add a startup and config system //

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands
+from config import token
 
 client = discord.Client(command_prefix = '$', intents=intents)
 
 bot.command()
 async def on_ready(ctx):
   print('Bot is now ready!')
+
+  
+  
+client.run(token)
